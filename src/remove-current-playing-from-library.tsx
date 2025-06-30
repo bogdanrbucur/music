4ee3cf78e12e0b3c76785f800683cfc06453610f
Bogdan Bucur <bogdanrbucur@gmail.com>
2025-06-30 19:42:55 +0300

    Add command to remove currently playing track from Library

---
+++ src/remove-current-playing-from-library.tsx
@@ -0,0 +1,47 @@
+import { closeMainWindow, showToast, Toast } from "@raycast/api";
+import { pipe } from "fp-ts/lib/function";
+import * as TE from "fp-ts/TaskEither";
+import * as music from "./util/scripts";
+
+export default async () => {
+  closeMainWindow();
+
+  await pipe(
+  // Get current track metadata
+  music.currentTrack.getCurrentTrackInfo(),
+
+  // Reveal the current track in Music
+  TE.chainFirst(() => pipe(
+    music.currentTrack.reveal,
+    TE.apFirst(music.general.activate),
+    // TE.chainFirst(() => TE.rightTask(() => new Promise((res) => setTimeout(res, 500))))
+  )),
+
+  // Remove from Library (remove the download first, if there is one)
+  TE.chainW((track) =>
+    pipe(
+      music.currentTrack.removeFromLibrary,
+      TE.map(() => track), // preserve original metadata
+    )
+  ),
+
+  // Show success toast
+  TE.map((track) =>
+    showToast({
+      style: Toast.Style.Success,
+      title: `Removed ${track.artist} - ${track.name} from Library.`,
+    })
+  ),
+
+  // Show failure toast
+  TE.mapLeft(() =>
+    showToast({
+      style: Toast.Style.Failure,
+      title: "Error while removing track from Library.",
+    })
+  ),
+
+  // Skip to next track
+  TE.chainW(() => music.player.next)
+)();
+};
